ab165d248e3b92dd62b5f60677bf24660dc2645a
Marc Magon <ged_marc@hotmail.com>
2017-06-16 18:56:05 +0200

    first commit

---
+++ src/za/co/mmagon/jwebswing/plugins/angularautofocus/AngularAutoFocusPageConfigurator.java
@@ -0,0 +1,57 @@
+package za.co.mmagon.jwebswing.plugins.angularautofocus;
+
+import za.co.mmagon.jwebswing.Page;
+import za.co.mmagon.jwebswing.PageConfigurator;
+import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
+import za.co.mmagon.jwebswing.plugins.PluginInformation;
+import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+
+/**
+ * Allows for elements to have an auto expand by adding a class
+ *
+ * @author Marc Magon
+ * @since 16 Jun 2017
+ */
+@PluginInformation(pluginName = "Angular Auto Focus",
+        pluginDescription = "Auto Focus Components with Angular",
+        pluginUniqueName = "jwebswing-angular-auto-focus",
+        pluginVersion = "1.0.0",
+        pluginCategories = "angular,focus,ui,web ui, framework",
+        pluginSubtitle = "Auto Focus Components with Angular",
+        pluginSourceUrl = "https://www.akveo.com/products.html",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-Angular-Auto-Focus/wiki",
+        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-Angular-Auto-Focus",
+        pluginIconUrl = "",
+        pluginIconImageUrl = "",
+        pluginOriginalHomepage = "https://www.akveo.com/products.html",
+        pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularAutoFocus.jar/download"
+)
+@ComponentInformation(name = "Angular Auto Focus",
+        description = "Auto Focus Components with Angular",
+        url = "https://www.akveo.com/products.html")
+public class AngularAutoFocusPageConfigurator extends PageConfigurator
+{
+
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * Constructs a new AngularAutoFocusPageConfigurator
+     */
+    public AngularAutoFocusPageConfigurator()
+    {
+        //Nothing needed
+    }
+
+    @Override
+    public Page configure(Page page)
+    {
+        if (!page.isConfigured())
+        {
+            JQueryPageConfigurator.setRequired(page.getBody(), true);
+            AngularPageConfigurator.setRequired(page.getBody(), true);
+            page.getAngular().getAngularDirectives().add(new AngularAutoFocusDirective());
+        }
+        return page;
+    }
+}
---
+++ src/za/co/mmagon/jwebswing/plugins/angularautofocus/autoFocus.js
@@ -0,0 +1,20 @@
+JW_APP_NAME.directive('autoFocus', autoFocus);
+
+function autoFocus($timeout, $parse) {
+    return {
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.autoFocus);
+            scope.$watch(model, function (value) {
+                if (value === true) {
+                    $timeout(function () {
+                        element[0].focus();
+                        element[0].select();
+                    });
+                }
+            });
+            element.bind('blur', function () {
+                scope.$apply(model.assign(scope, false));
+            });
+        }
+    };
+}
