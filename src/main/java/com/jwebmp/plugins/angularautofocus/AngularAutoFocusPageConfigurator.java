2fcc67e83eb8bcad27206df2ad0de33b21bd3582
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:29 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/angularautofocus/AngularAutoFocusPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularautofocus/AngularAutoFocusPageConfigurator.java
@@ -76,7 +76,7 @@ public class AngularAutoFocusPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
